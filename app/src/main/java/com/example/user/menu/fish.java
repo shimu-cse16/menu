bfbbdb057d70cbe8d5f29eb6d58a25b0dafff7da
shimu-cse16 <shimu.cse.just@gmail.com>
2019-04-05 11:32:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/user/menu/fish.java
@@ -0,0 +1,93 @@
+package com.example.user.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class fish extends AppCompatActivity {
+
+    private ImageView view1;
+    private TextView T1;
+    private TextView  V1;
+    private Button B1;
+    private ImageView view2;
+    private TextView  T2;
+    private TextView  V2;
+    private Button   B2;
+    private ImageView view3;
+    private TextView  T3;
+    private TextView  V3;
+    private Button   B3;
+    private ImageView view4;
+    private TextView  T4;
+    private TextView  V4;
+    private Button   B4;
+    private ImageView view5;
+    private TextView  T5;
+    private TextView  V5;
+    private Button   B5;
+    private ImageView view6;
+    private TextView  T6;
+    private TextView  V6;
+    private Button   B6;
+    private ImageView view7;
+    private TextView  T7;
+    private TextView  V7;
+    private Button   B7;
+    private ImageView view8;
+    private TextView  T8;
+    private TextView  V8;
+    private Button   B8;
+    private ImageView view9;
+    private TextView  T9;
+    private TextView  V9;
+    private Button   B9;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fish);
+
+
+        view1=(ImageView) findViewById(R.id.image1);
+        T1=(TextView) findViewById(R.id.t1);
+        V1=(TextView) findViewById(R.id.v1);
+        B1=(Button) findViewById(R.id.b1);
+        view2=(ImageView) findViewById(R.id.image2);
+        T2=(TextView) findViewById(R.id.t2);
+        V2=(TextView) findViewById(R.id.v2);
+        B2=(Button) findViewById(R.id.b2);
+        view3=(ImageView) findViewById(R.id.image3);
+        T3=(TextView) findViewById(R.id.t3);
+        V3=(TextView) findViewById(R.id.v3);
+        B3=(Button) findViewById(R.id.b3);
+        view4=(ImageView) findViewById(R.id.image4);
+        T4=(TextView) findViewById(R.id.t4);
+        V4=(TextView) findViewById(R.id.v4);
+        B4=(Button) findViewById(R.id.b4);
+        view5=(ImageView) findViewById(R.id.image5);
+        T5=(TextView) findViewById(R.id.t5);
+        V5=(TextView) findViewById(R.id.v5);
+        B5=(Button) findViewById(R.id.b5);
+        view6=(ImageView) findViewById(R.id.image6);
+        T6=(TextView) findViewById(R.id.t6);
+        V6=(TextView) findViewById(R.id.v6);
+        B6=(Button) findViewById(R.id.b6);
+        view7=(ImageView) findViewById(R.id.image7);
+        T7=(TextView) findViewById(R.id.t7);
+        V7=(TextView) findViewById(R.id.v7);
+        B7=(Button) findViewById(R.id.b7);
+        view8=(ImageView) findViewById(R.id.image8);
+        T8=(TextView) findViewById(R.id.t8);
+        V8=(TextView) findViewById(R.id.v8);
+        B8=(Button) findViewById(R.id.b8);
+        view9=(ImageView) findViewById(R.id.image9);
+        T9=(TextView) findViewById(R.id.t9);
+        V9=(TextView) findViewById(R.id.v9);
+        B9=(Button) findViewById(R.id.b9);
+    }
+}
---
+++ app/src/main/java/com/example/user/menu/shrimp.java
@@ -0,0 +1,68 @@
+package com.example.user.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class shrimp extends AppCompatActivity {
+
+
+    private ImageView view1;
+    private TextView T1;
+    private TextView  V1;
+    private Button B1;
+    private ImageView view2;
+    private TextView  T2;
+    private TextView  V2;
+    private Button   B2;
+    private ImageView view3;
+    private TextView  T3;
+    private TextView  V3;
+    private Button   B3;
+    private ImageView view4;
+    private TextView  T4;
+    private TextView  V4;
+    private Button   B4;
+    private ImageView view5;
+    private TextView  T5;
+    private TextView  V5;
+    private Button   B5;
+    private ImageView view6;
+    private TextView  T6;
+    private TextView  V6;
+    private Button   B6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shrimp);
+
+
+        view1=(ImageView) findViewById(R.id.image1);
+        T1=(TextView) findViewById(R.id.t1);
+        V1=(TextView) findViewById(R.id.v1);
+        B1=(Button) findViewById(R.id.b1);
+        view2=(ImageView) findViewById(R.id.image2);
+        T2=(TextView) findViewById(R.id.t2);
+        V2=(TextView) findViewById(R.id.v2);
+        B2=(Button) findViewById(R.id.b2);
+        view3=(ImageView) findViewById(R.id.image3);
+        T3=(TextView) findViewById(R.id.t3);
+        V3=(TextView) findViewById(R.id.v3);
+        B3=(Button) findViewById(R.id.b3);
+        view4=(ImageView) findViewById(R.id.image4);
+        T4=(TextView) findViewById(R.id.t4);
+        V4=(TextView) findViewById(R.id.v4);
+        B4=(Button) findViewById(R.id.b4);
+        view5=(ImageView) findViewById(R.id.image5);
+        T5=(TextView) findViewById(R.id.t5);
+        V5=(TextView) findViewById(R.id.v5);
+        B5=(Button) findViewById(R.id.b5);
+        view6=(ImageView) findViewById(R.id.image6);
+        T6=(TextView) findViewById(R.id.t6);
+        V6=(TextView) findViewById(R.id.v6);
+        B6=(Button) findViewById(R.id.b6);
+    }
+}
